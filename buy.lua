c35696adc82827531163e2eb927adc7c3db12350
Luciel <jpmizidoro@gmail.com>
2024-05-31 16:41:35 -0300

    Update buy.lua

--- buy.lua
+++ buy.lua
@@ -3,7 +3,7 @@ local itemPrice = 100.00
 local function checkPrice ()
     print("How much money have you got?")
     io.write("> ")
-    local playerMoney = toNumber(io.read())
+    local playerMoney = tonumber(io.read())
 
     if playerMoney and playerMoney >= itemPrice then
         return true
@@ -16,9 +16,9 @@ local function main ()
     local buy = checkPrice()
     
     if buy then
-        print("Você adquiriu o item!")
+        print("Voce adquiriu o item! Fundos:")
     else
-        print("Sem fundos suficientes.")
+        print("Sem fundos suficientes. Fundos:")
     end
 end
 
